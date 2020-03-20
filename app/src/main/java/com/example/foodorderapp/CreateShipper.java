5b4a646ced33cb5b5cd0658579f2f471fd36c5f9
HASINI <oshanivinodya.98@gmail.com>
2020-03-20 18:27:25 +0530

    Successful

---
+++ app/src/main/java/com/example/foodorderapp/CreateShipper.java
@@ -0,0 +1,6 @@
+package com.example.foodorderapp;
+
+import android.app.Activity;
+
+public class CreateShipper extends Activity {
+}
---
+++ app/src/main/java/com/example/foodorderapp/ShoppngCart.java
@@ -0,0 +1,6 @@
+package com.example.foodorderapp;
+
+import android.app.Activity;
+
+public class ShoppngCart extends Activity {
+}
